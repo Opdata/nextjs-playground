bcc4a2e841208c0afd2bc0d22ebc40afbd07c6e9
Opdata <pb03641@gmail.com>
2022-03-29 23:04:13 +0900

    Dynamic Routing Query Parameter Data Test

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,11 @@
 // /news/newsId
 
+import { useRouter } from 'next/router';
+
 function DetailPage() {
+  const newsId = useRouter().query.newsId; // 해당 JS 파일의 [] 안에 newsId 라고 써있기때문에 query.newsId, /news/1 로 접속시 newsId 는 1이 된다.
+  console.log(newsId);
+
   return <h1>The Detail Page</h1>;
 }
 
