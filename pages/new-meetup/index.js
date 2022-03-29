c14e1e18900324b6038dfd8728f754d58abb94db
Opdata <pb03641@gmail.com>
2022-03-30 00:06:40 +0900

    Home Component Modify(import MeetupList, Dummy Data Add) & _app.js Header Component(Layout Component Add)

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,11 @@
+import AddMeetUpForm from '../../components/meetups/NewMeetupForm';
+
+function NewMeetUpForm() {
+  const AddMeetUpHandler = (enteredMeetUpData) => {
+    console.log(enteredMeetUpData);
+  };
+
+  return <AddMeetUpForm onAddMeetup={AddMeetUpHandler} />;
+}
+
+export default NewMeetUpForm;
