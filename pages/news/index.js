5a735f2e8fecff1b707b4f5ee195de380f4bb5f7
Opdata <pb03641@gmail.com>
2022-03-29 23:16:24 +0900

    Page Link Test

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,23 @@
 //  /news
 
+import Link from 'next/link';
+
 function newsPage() {
-  return <h1>The News Page</h1>;
+  // Link는 앵커 태그를 렌더하지만 클릭시 브라우저의 새 HTML 요청을 막고 로드된 컴포넌트를 로드하고 URL을 바꿈, 마치 페이지를 바꾼 것 처럼
+
+  return (
+    <>
+      <h1>The News Page</h1>;
+      <ul>
+        <li>
+          <Link href="/news/Something">NextJS Something</Link>
+        </li>
+        <li>
+          <Link href="/">Return Home</Link>
+        </li>
+      </ul>
+    </>
+  );
 }
 
 export default newsPage;
